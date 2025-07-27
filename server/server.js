02237cd7e0499eaf216bcbac95e2077d378afe20
aryansenpaii <aryansenpai256@gmail.com>
2025-07-28 01:52:18 +0530

    Added initial Home route in server.js (for deploying online)

--- server/server.js
+++ server/server.js
@@ -1,32 +1,40 @@
-const express= require('express')
-const cors = require('cors')
-const dotenv= require('dotenv')
-const colors= require('colors')
-const morgan= require('morgan')
-const connectDB = require('./config/db')
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
 
 //DOTENV
-dotenv.config()
+dotenv.config();
 
 //REST OBJECT
-const app= express()
+const app = express();
 
 //MONGODB
 connectDB();
 
 //middleware
-app.use(cors())
-app.use(cors())
-app.use(morgan('dev'))
+app.use(cors());
+app.use(cors());
+app.use(morgan("dev"));
 app.use(express.json());
 //ROUTES
-app.use("/api/v1/auth", require("./routes/userRoutes"))
-app.use("/api/v1/post",require("./routes/postRoutes"))
+app.use("/api/v1/auth", require("./routes/userRoutes"));
+app.use("/api/v1/post", require("./routes/postRoutes"));
+
+//home
+app.get("/", (req, res) => {
+  res.status(200).send({
+    success: true,
+    msg: "Node server Running!!",
+  });
+});
 
 //PORT
-const PORT= process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
 //LISTENING
-app.listen(PORT, ()=>{
-    console.log(`Server is running on ${PORT}`.bgCyan.white);
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`.bgCyan.white);
+});
